6ed65958d7925628c6df32ab68087de31ea452f5
bryanhorus <bryan.alva132@gmail.com>
2020-09-22 17:32:57 -0500

    creacion vistA LOGIN

---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="margin: 0 auto; width: 35%">
+    <Panel>
+      <div>
+        <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <br />
+      <div>
+        <span class="p-input-icon-left">
+          <i class="pi pi-user" />
+          <InputText type="text" v-model="value1" placeholder="Usuario" />
+        </span>
+      </div>
+      <br />
+      <div>
+      <span class="p-input-icon-left">
+        <i class="pi pi-key" />
+        <InputText type="password" v-model="value1" placeholder="Contraseña" />
+      </span>
+      </div>
+      <br />
+      <div>
+      <Button label="Ingresar" icon="pi pi-check" />
+      </div>
+      <br />
+    </Panel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+ 
+ <style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import InputText from 'primevue/inputtext';
+import Panel from 'primevue/panel';
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/nova/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 Vue.config.productionTip = false
+Vue.component('InputText', InputText);
+Vue.component('Panel', Panel);
+Vue.component('Button', Button);
 
 new Vue({
   render: h => h(App),
